e0fccc4aa57ccc071aaa20b42aa82572833f0106
Renata-CDB <renatabianchine2016@gmail.com>
2022-05-19 15:45:07 -0300

    first commit

---
+++ APIStudy/Controllers/ClientController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIStudy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "My API in .Net Core in Framework 5.0!";
+        }
+    }
+}
